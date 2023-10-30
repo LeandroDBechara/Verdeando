2e7e6ba78d70ab359e1a27b8c4bab1c9b6c75fab
Marisol Barraza <marisolbarraza08@gmail.com>
2023-10-30 11:54:28 -0300

    carga de fuentes

--- Verdeando Colab/verdeando_colab/App.js
+++ Verdeando Colab/verdeando_colab/App.js
@@ -1,20 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {Text, View, Button } from 'react-native';
+import styles from './src/styles/inicioSesion';
+import Titulo from './src/components/titulo';
+
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hola Marisol</Text>
+      <titulo/>
       <StatusBar style="auto" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ Verdeando Colab/verdeando_colab/src/styles/inicioSesion.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: 'pink',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    header:{
+        color:'blue',
+        fontSize:28,
+    }
+});
+export default styles;
