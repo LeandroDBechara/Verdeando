51b2dd15362c87448f8aa19aba56b17a041e399c
Marisol Barraza <marisolbarraza08@gmail.com>
2023-10-30 22:35:04 -0300

    carga de fuentes

--- Verdeando Colab/verdeando_colab/App.js
+++ Verdeando Colab/verdeando_colab/App.js
@@ -1,15 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import {Text, View, Button } from 'react-native';
-import styles from './src/styles/inicioSesion';
-import Titulo from './src/components/titulo';
-
-
+import { StyleSheet, View, Text, } from 'react-native'
+import React, {useEffect, useState} from 'react'
+import * as Font from 'expo-font'
 
 export default function App() {
+  const [fontsLoaded, setFontsLoaded]= useState(false);
+    useEffect (() =>{
+      if(!fontsLoaded){
+        loadfonts();
+      } 
+    });
+  const loadfonts = async () =>{
+    await Font.loadAsync({
+      'pressStart': require('./assets/fonts/PressStart.ttf',)
+    });
+  setFontsLoaded(true);
+  }
+  if (!fontsLoaded){
+    return(<View/>)
+  }
   return (
-    <View style={styles.container}>
-      <titulo/>
-      <StatusBar style="auto" />
+    <View>
+      <Text style={{fontFamily: 'pressStart'}}>App</Text>
     </View>
-  );
+  )
 }
